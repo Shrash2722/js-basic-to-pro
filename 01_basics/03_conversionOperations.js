50352ed53b49310e6eeaed2f28ead677b81cfde6
Shrash2722 <97334117+Shrash2722@users.noreply.github.com>
2025-10-02 13:48:42 +0000

    operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,57 @@
+let score = "33"
+let anoterScore = "33ab"
+let nullScore  = null;
+let undefineScore = undefined;
+let booleanScore = true;
+let stringScore = "Shraddha";
+
+let valueInNumber = Number(score); 
+let valueInAnotherNumber = Number(anoterScore); 
+let valueInNullNumber = Number(nullScore); 
+let valueInUndefinedNumber = Number(undefineScore);
+let valueInBooleaNumber = Number(booleanScore);
+let valueInStringNumber = Number(stringScore);
+
+/*
+console.log(typeof valueInNumber); // number
+
+console.log(typeof valueInAnotherNumber); // number
+console.log(valueInAnotherNumber); // NaN
+
+console.log(typeof valueInNullNumber); // number
+console.log(valueInNullNumber); // 0
+
+console.log(typeof valueInUndefinedNumber); // number
+console.log(valueInUndefinedNumber); // NaN
+
+console.log(typeof valueInBooleaNumber); // number
+console.log(valueInBooleaNumber); // 1
+
+console.log(typeof valueInStringNumber); // number
+console.log(valueInStringNumber); // NaN
+*/
+
+let loggedIn = 1;
+
+let booleanValueOfLoggedIn = Boolean(loggedIn);
+
+console.log(typeof booleanValueOfLoggedIn);
+console.log(booleanValueOfLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false ; "value" => true
+
+let someNumber = 33;
+let numberToString = String(someNumber);
+
+console.log(typeof numberToString); // string
+console.log(numberToString); // 33
+
+// ++++++++++++++++++++++ Operations +++++++++++++++++++++++++++++
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); // 32
+
+
+
+
+
